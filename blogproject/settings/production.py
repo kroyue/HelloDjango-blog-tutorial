6daac27ebae71734b984998003d8ad16b834d0d2
kroyue <1148504169@qq.com>
2020-12-29 03:58:11 +0800

    只留下域名

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -2,4 +2,4 @@ from .common import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
-ALLOWED_HOSTS = ['kroyue.com','localhost','127.0.0.1','121.196.161.14']
+ALLOWED_HOSTS = ['kroyue.com']
