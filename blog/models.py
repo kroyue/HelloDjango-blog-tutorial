77738846a9b0cf96d6cb85cd8883b49d9ec75a10
kroyue <1148504169@qq.com>
2021-01-05 16:56:44 +0800

    添加RSS订阅功能

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+
+import re
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
+from django.utils.functional import cached_property
 # Create your models here.
 
 class Category(models.Model):
@@ -64,3 +69,30 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    @property
+    def toc(self):
+        return self.rich_content.get("toc", "")
+
+    @property
+    def body_html(self):
+        return self.rich_content.get("content", "")
+
+    @cached_property
+    def rich_content(self):
+        return generate_rich_content(self.body)
+
+
+def generate_rich_content(value):
+    md = markdown.Markdown(
+        extensions=[
+            "markdown.extensions.extra",
+            "markdown.extensions.codehilite",
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ]
+    )
+    content = md.convert(value)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    toc = m.group(1) if m is not None else ""
+    return {"content": content, "toc": toc}
